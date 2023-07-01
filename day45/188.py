cd59239b60dde8d80920d34216131726996b0dc9
heymamba824 <heymamba824@gmail.com>
2023-07-01 14:59:33 +0800

    day45

--- day45/188.py
+++ day45/188.py
@@ -17,3 +17,17 @@ class Solution:
         return dfs(0,False,0)
         
 
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        #dp[i][0]:  hold the stock 
+        #dp[i][1]: buy the stock
+        #dp[i][2]: sell the stock  
+        dp = [[0] * (2*k+1) for _ in range(len(prices))]
+        for i in range(1,2*k,2):
+            dp[0][i] = -prices[0]
+        for i in range(1,len(prices)):
+            for j in range(0,2*k,2):
+                dp[i][0] = dp[i-1][0]
+                dp[i][j+1] = max(dp[i-1][j+1],dp[i-1][j]-prices[i])
+                dp[i][j+2] = max(dp[i-1][j+2],dp[i-1][j+1]+prices[i])
+        return dp[-1][-1]
